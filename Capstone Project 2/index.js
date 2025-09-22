d010441bd7521f3dec4b6a6d755ba39abe59abe8
Yulomja <piazza.loredana@hotmail.com>
2025-09-22 08:52:25 +0200

    spyscroll and margin inssue solved

---
+++ Capstone Project 2/index.js
@@ -0,0 +1,15 @@
+// document.addEventListener("DOMContentLoaded", () => {
+//     const navLinks = document.querySelectorAll(".nav-link");
+
+//     navLinks.forEach(link => {
+//         link.addEventListener("click", function (e) {
+//             // e.preventDefault(); // stop page jumping if using #
+
+//             // remove active styles
+//             navLinks.forEach(l => l.classList.remove("active"));
+
+//             // add active styles
+//             this.classList.add("active");
+//         });
+//     });
+// });
